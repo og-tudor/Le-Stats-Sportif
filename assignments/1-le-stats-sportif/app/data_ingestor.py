0cd886833adc219e10ed47b36b3b75432678bbd9
Tudor Frecus <tudor.frecus@gmail.com>
2024-04-02 15:13:12 +0300

    Add DataStore class and modify DataIngestor constructor

--- assignments/1-le-stats-sportif/app/data_ingestor.py
+++ assignments/1-le-stats-sportif/app/data_ingestor.py
@@ -1,9 +1,23 @@
 import os
 import json
+import csv
+
+class DataStore:
+    def __init__(self, data: dict):
+        self.data = data
+    
+    def get_data(self, question: str) -> dict:
+        return self.data[question]
+    
+    def insert_data(self, question: str, data: list):
+        self.data[question] = data
+    
 
 class DataIngestor:
-    def __init__(self, csv_path: str):
+    def __init__(self, csv_path: str, data_store: DataStore):
         # TODO: Read csv from csv_path
+        self.csv_path = csv_path
+        self.data_store = data_store
 
         self.questions_best_is_min = [
             'Percent of adults aged 18 years and older who have an overweight classification',
